e22639a98b557af74d056b3e6bea641be48fe33f
hebibov666 <asuslap503@gmail.com>
2025-02-02 01:37:41 +0400

    Full deploy 2

--- components/FavoriteProducts.js
+++ components/FavoriteProducts.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSelector,useDispatch } from "react-redux";
 import Card from '../ui-components/Card';
 import { getProducts } from '@/redux/productSlice';
